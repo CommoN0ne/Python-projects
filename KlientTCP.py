461074d4808df0be3a48b682a9ed062f3c2be6cc
CommoN0ne <37288523+CommoN0ne@users.noreply.github.com>
2023-12-09 06:11:11 -0500

    Add files via upload

---
+++ KlientTCP.py
@@ -0,0 +1,19 @@
+import socket
+
+target_host = "localhost"
+target_port = 21
+
+#create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect to the client
+client.connect((target_host, target_port))
+
+#send some data
+client.send(b"dupa123")
+
+#recive some data
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
